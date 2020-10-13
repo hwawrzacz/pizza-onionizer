3a13c12e9a662dc46005d916ae8a2657a8ff9178
Hubert <hwawrzacz.dev@gmail.com>
2020-10-13 18:41:46 +0200

    Split components into *.html and *.css files

--- src/components/label-editable/EditableLabel.vue
+++ src/components/label-editable/EditableLabel.vue
@@ -1,17 +1,4 @@
-<template>
-  <div class="editable-label-wrapper fx-col fx-h-center">
-    <label for="price">{{ label }}</label>
-    <input
-      :class="{ disabled: !editMode }"
-      :placeholder="placeholder"
-      :value="value"
-      @focusout="onValueChanged"
-    />
-    <span :class="{ disabled: editMode }" @click="enableEditMode">{{
-      value
-    }}</span>
-  </div>
-</template>
+<template src="./EditableLabel.html" />
 
 <script>
 export default {
@@ -32,7 +19,7 @@ export default {
         this.disableEditMode();
       }
     },
-
+  
     enableEditMode(event) {
       this.editMode = true;
       event.target.previousSibling.focus();
@@ -49,35 +36,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.editable-label-wrapper {
-  margin: 6px 8px;
-}
-
-.disabled {
-  padding: 0;
-  max-height: 0;
-  opacity: 0;
-  border: none;
-}
-
-input,
-span {
-  box-sizing: border-box;
-  display: block;
-  padding: 2px 6px;
-
-  overflow: hidden;
-  color: inherit;
-  font-size: 1.5em;
-  font-weight: bold;
-  border: none;
-  text-align: center;
-  cursor: text;
-}
-
-span {
-  margin: 0 6px;
-  padding: 2px 0;
-}
-</style>
+<style scoped src="./EditableLabel.css" />
