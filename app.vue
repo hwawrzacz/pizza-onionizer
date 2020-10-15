4ed38fac1a7629794cb38ec5dde0b1958561800f
Hubert <hwawrzacz.dev@gmail.com>
2020-10-15 22:04:40 +0200

    Rename App -> app pt. 2/2

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template src="./app.html"/>
+<script>
+import HomePage from "./pages/home-page/home-page.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomePage
+  }
+};
+</script>
+<style lang="scss" src="./app.scss" />
